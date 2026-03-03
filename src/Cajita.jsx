ab9c54b5ec5f0876f9cabaecdc7c3aa363429e6b
Rey-Orellana <reyorellana13@gmail.com>
2026-03-02 23:59:33 -0400

    Facil

--- src/Cajita.jsx
+++ src/Cajita.jsx
@@ -4,6 +4,9 @@ export const Cajita = () => {
   return (
     <>
       <h1>Facilito</h1>
+      <div class="item 1">1</div>
+      <div class="item 2">2</div>
+      <div class="item 3">3</div>
     </>
   )
 }
