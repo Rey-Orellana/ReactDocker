def1c90e402e85a4f4afe30304e3f53097b9e796
Rey-Orellana <reyorellana13@gmail.com>
2026-03-02 23:53:20 -0400

    Haciendo un commit como formulario

---
+++ src/ComponenteForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const ComponenteForm = () => {
+  return (
+    <>
+     <div>
+        <form action="">
+            <label htmlFor="">Nombre</label>
+            <input type="text" />
+            <br />
+            <label htmlFor="">Contraseña</label>
+            <input type="password" />
+        </form>
+     </div>
+    </>
+  )
+}
