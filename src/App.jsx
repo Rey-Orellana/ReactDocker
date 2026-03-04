9ad343dc3d903d64a187519cad53ea7eb7197c72
Rey-Orellana <reyorellana13@gmail.com>
2026-03-03 23:35:51 -0400

    Agregando cosas para aprender cada dia

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Componentesito } from './Componentesito'
-import { Cajita } from './Cajita'
 import './App.css'
-import { Cajita } from './Cajita'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>HOLA A TODOS CHICOS MALOS</h1>
-    <Componentesito></Componentesito>
-    <p>Estamos aprendiendo a usar Docker we</p>
-    <Cajita></Cajita>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Cajita></Cajita>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR SOPENCO
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more MAS 
-      </p>
-      <h2>Agregnado cosas al azar</h2>
+    <h1>Este es un componente</h1>
+    <p>Este es un parrafo we</p>
     </>
   )
 }
