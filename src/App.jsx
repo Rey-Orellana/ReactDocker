6255006ac58cc9d55150fc7f2edb1315f44b2a12
Rey-Orellana <reyorellana13@gmail.com>
2026-03-03 23:28:18 -0400

    Agregando a la cajita a mi codigo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Componentesito } from './Componentesito'
 import './App.css'
+import { Cajita } from './Cajita'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Cajita></Cajita>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- src/Cajita.jsx
+++ src/Cajita.jsx
@@ -4,6 +4,7 @@ export const Cajita = () => {
   return (
     <>
       <h1>Facilito</h1>
+      <p>Agregando un parrafo a mi cosita</p>
     </>
   )
 }
