af512c2ad38bbb565902f86a29cc844c92fc4020
Rey-Orellana <reyorellana13@gmail.com>
2026-03-04 23:42:08 -0400

    Agregando una lista

---
+++ src/Lista.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Lista = () => {
+  return (
+    <>
+     <ul>
+        <li>Coffe</li>
+        <li>Tea</li>
+        <li>Milk</li>
+     </ul>
+    </>
+  )
+}
